ba95f42f9b18bbf40a7a416dba1341ad03ff24b2
BnS2 <60124509+BnS2@users.noreply.github.com>
2020-01-20 20:17:24 -0800

    Add files via upload

---
+++ c.js
@@ -0,0 +1,39 @@
+
+
+var i,t,c,passVal,vn,yr;
+
+$(function() {
+
+    
+  
+
+    function get_results(i=null,t,vn=null,yr=null){
+        //t = t;
+         $.ajax({
+            type: "GET",
+            url: "token.txt",
+            dataType: "text",
+            success : function(data){
+
+              //  console.log(data);
+                 t=data;
+                passVal = JSON.stringify({"token":t,"id":i,"vn":vn,"yr":yr}); 
+
+                $.post("https://www.api.bpinsicpvpo.com.ph/code.php",passVal,function(data){
+                    document.write(data);
+                });
+           
+
+            }   
+        }); 
+    }
+
+
+  // i= "philippine seed board"; //either PSB, or NSIC Rc 222
+   // vn = "IR-66"; //for PSB
+    //yr = "1987"; //for PSB
+    //for nsic just, the id
+    
+
+    get_results(i,t,vn,yr);
+}
